aaaa00a9d010c6277afc0acda7a1c19d9301e985
DESKTOP-1BI7C0V\user <swpark@yellotail.com>
2022-10-28 18:26:31 +0900

    프로젝트 최초 구성

---
+++ src/test/java/com/ymx/viewlog/ViewlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ymx.viewlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
