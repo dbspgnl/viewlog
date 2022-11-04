446e6c0c69f1b8b1e6a281b21ce9ca663654e9d9
DESKTOP-1BI7C0V\user <swpark@yellotail.com>
2022-11-04 18:19:36 +0900

    + logging 모듈화 / logback 설정 및 application profile 설정 + slack webhook 설정 (error level)

--- src/main/java/com/ymx/viewlog/controller/ViewlogController.java
+++ src/main/java/com/ymx/viewlog/controller/ViewlogController.java
@@ -3,11 +3,20 @@ package com.ymx.viewlog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class ViewlogController {
 
     @RequestMapping("/")
     public String root(){
+        return "index";
+    }
+
+    @RequestMapping("/errorlog")
+    public String index(){
+        log.error("error log test!");
         return "index";
     }
     
